6bbd80bee81e04355c42418e00767ac956468ede
Suraj Singla <surajsingla333@gmail.com>
2026-06-19 15:05:19 +0530

    package update

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -6,3 +6,25 @@ export * from './payment.schema'
 export * from './review.schema'
 export * from './discount.schema'
 export * from './notification.schema'
+
+import * as userSchemas from './user.schema'
+import * as bookingSchemas from './booking.schema'
+import * as activitySchemas from './activity.schema'
+import * as slotSchemas from './slot.schema'
+import * as paymentSchemas from './payment.schema'
+import * as reviewSchemas from './review.schema'
+import * as discountSchemas from './discount.schema'
+import * as notificationSchemas from './notification.schema'
+
+const beamSchemas = {
+  ...userSchemas,
+  ...bookingSchemas,
+  ...activitySchemas,
+  ...slotSchemas,
+  ...paymentSchemas,
+  ...reviewSchemas,
+  ...discountSchemas,
+  ...notificationSchemas,
+}
+
+export default beamSchemas
